465ebfdb8f992f41010a0458e509abb09adc4cff
Leo <leo.ghafari@gmail.com>
2020-04-02 21:35:40 +0900

    Explicitly use the ctol namespace

--- ctol/cell_list.hpp
+++ ctol/cell_list.hpp
@@ -20,53 +20,53 @@ struct cell_list<>
 template<typename CellListA, typename CellListB>
 struct concatenate;
 template<typename... As, typename... Bs>
-struct concatenate<cell_list<As...>, cell_list<Bs...>>
+struct concatenate<ctol::cell_list<As...>, ctol::cell_list<Bs...>>
 {
-    using type = cell_list<As..., Bs...>;
+    using type = ctol::cell_list<As..., Bs...>;
 };
 
 template<typename CellList, typename Cell>
 struct append;
 template<typename... Ts, typename C>
-struct append<cell_list<Ts...>, C>
+struct append<ctol::cell_list<Ts...>, C>
 {
-    using type = cell_list<Ts..., C>;
+    using type = ctol::cell_list<Ts..., C>;
 };
 
 template<typename CellList, std::size_t N>
 struct get_n_first_cells;
 template<typename T, typename... Ts>
-struct get_n_first_cells<cell_list<T, Ts...>, 1>
+struct get_n_first_cells<ctol::cell_list<T, Ts...>, 1>
 {
-    using type = cell_list<T>;
+    using type = ctol::cell_list<T>;
 };
 template<std::size_t N>
-struct get_n_first_cells<cell_list<>, N>
+struct get_n_first_cells<ctol::cell_list<>, N>
 {
-    using type = cell_list<>;
+    using type = ctol::cell_list<>;
 };
 template<typename T, typename... Ts, std::size_t N>
-struct get_n_first_cells<cell_list<T, Ts...>, N>
+struct get_n_first_cells<ctol::cell_list<T, Ts...>, N>
 {
-    using type =
-        typename concatenate<cell_list<T>,
-                             typename get_n_first_cells<cell_list<Ts...>, N - 1>::type>::type;
+    using type = typename concatenate<
+        ctol::cell_list<T>,
+        typename ctol::get_n_first_cells<ctol::cell_list<Ts...>, N - 1>::type>::type;
 };
 
 template<typename CellList, std::size_t N>
 struct get_n_last_cells;
 template<typename T, typename... Ts, std::size_t N>
-struct get_n_last_cells<cell_list<T, Ts...>, N>
+struct get_n_last_cells<ctol::cell_list<T, Ts...>, N>
 {
     using type =
         typename std::conditional<N == sizeof...(Ts) + 1,
-                                  cell_list<T, Ts...>,
-                                  typename get_n_last_cells<cell_list<Ts...>, N>::type>::type;
+                                  ctol::cell_list<T, Ts...>,
+                                  typename get_n_last_cells<ctol::cell_list<Ts...>, N>::type>::type;
 };
 template<std::size_t N>
-struct get_n_last_cells<cell_list<>, N>
+struct get_n_last_cells<ctol::cell_list<>, N>
 {
-    using type = cell_list<>;
+    using type = ctol::cell_list<>;
 };
 
 template<typename CellList, std::size_t Index>
@@ -75,25 +75,25 @@ struct get
     using type = void;
 };
 template<typename T, typename... Ts>
-struct get<cell_list<T, Ts...>, 0>
+struct get<ctol::cell_list<T, Ts...>, 0>
 {
     using type = T;
 };
 template<typename T, typename... Ts, std::size_t Index>
 struct get<cell_list<T, Ts...>, Index>
 {
-    using type = typename get<cell_list<Ts...>, Index - 1>::type;
+    using type = typename ctol::get<ctol::cell_list<Ts...>, Index - 1>::type;
 };
 
 template<typename CellList, std::size_t Index, typename T>
 struct set
 {
     private:
-    using front_cells = get_n_first_cells<CellList, Index>::type;
-    using back_cells = get_n_last_cells<CellList, CellList::size - (Index + 1)>::type;
+    using front_cells = ctol::get_n_first_cells<CellList, Index>::type;
+    using back_cells = ctol::get_n_last_cells<CellList, CellList::size - (Index + 1)>::type;
 
     public:
-    using type = concatenate<typename append<front_cells, T>::type, back_cells>::type;
+    using type = ctol::concatenate<typename ctol::append<front_cells, T>::type, back_cells>::type;
 };
 
 } // namespace ctol
